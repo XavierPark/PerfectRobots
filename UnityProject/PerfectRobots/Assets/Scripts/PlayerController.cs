1d4b2827d2cd177afdd5ff375fb80c681f9a36ed
jaidon82 <jaidon82@gmail.com>
2023-10-28 13:47:02 -0400

    Character controller and camera Functional

--- UnityProject/PerfectRobots/Assets/Scripts/PlayerController.cs
+++ UnityProject/PerfectRobots/Assets/Scripts/PlayerController.cs
@@ -10,15 +10,19 @@ public class PlayerController : MonoBehaviour
     [Header("----- Player Stats -----")]
     [Range(-10, -40)][SerializeField] float gravityValue;
     [Range(2, 8)][SerializeField] float playerSpeed;
-
+    [SerializeField] float jumpHeight;
+    [Range(1,10)][SerializeField] float HP;
     [Header("----- Gun Stats -----")]
     [SerializeField] int shootDamage;
     [SerializeField] int shootDist;
     [SerializeField] int shootRate;
+    [SerializeField] GameObject bullet;
 
     private Vector3 move;
     private Vector3 playerVelocity;
     bool isShooting;
+    private bool groundedPlayer;
+    private int jumpTimes;
 
     void Start()
     {
@@ -28,35 +32,65 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist, Color.red);
+
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * shootDist);
 
         if (Input.GetButton("Shoot") && !isShooting)
+        {
             StartCoroutine(shoot());
+        }
 
-        move = Input.GetAxis("Horizontal") * transform.right +
-              Input.GetAxis("Vertical") * transform.forward;
+
+
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+            jumpTimes = 0;
+        }
+
+        Vector3 move = Input.GetAxis("Horizontal") * transform.right +
+                        Input.GetAxis("Vertical") * transform.forward;
         controller.Move(move * Time.deltaTime * playerSpeed);
+
+
+
+        // Changes the height position of the player..
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y = jumpHeight;
+            jumpTimes++;
+
+
+
+        }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
     }
-
     IEnumerator shoot()
     {
         isShooting = true;
-
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out hit, shootDist))
         {
+            Instantiate(bullet, hit.point, bullet.transform.rotation);
             IDamage damageable = hit.collider.GetComponent<IDamage>();
 
             if (hit.transform != transform && damageable != null)
             {
                 damageable.takeDamage(shootDamage);
-            }
-        }
 
+            }
+
+
+        }
         yield return new WaitForSeconds(shootRate);
         isShooting = false;
+
+    }
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
     }
 }
