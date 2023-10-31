82de8981c84fc19b1606e4260d168fb7216783e3
christian dall <Christian.j.dall@gmail.com>
2023-10-31 15:05:10 -0400

    buttons scrips

---
+++ UnityProject/PerfectRobots/Assets/Scripts/Buttons.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public void resume()
+    {
+        GameManager.Instance.stateUnpause();
+    }
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        GameManager.Instance.stateUnpause();
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
