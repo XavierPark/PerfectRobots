2d5f40ecb78fc86ae4fa91440c5fe4d3c68a6064
Damien Meadows <KEMeadows@student.fullsail.edu>
2023-11-16 00:29:02 -0500

    Revert "Revert "still trying""
    This reverts commit e6cbc186adc5235a566d3fa82dd750824432ce40.

--- UnityProject/PerfectRobots/Assets/Scripts/LogansScene/ShieldStats.cs
+++ UnityProject/PerfectRobots/Assets/Scripts/LogansScene/ShieldStats.cs
@@ -7,6 +7,7 @@ public class ShieldStats : MonoBehaviour, IDamage
 {
     [Header("===== Components =====")]
     [SerializeField] MeshRenderer model;
+    [SerializeField] GameObject shieldPos;
 
     [SerializeField] GameObject shieldPos;
     [SerializeField] AudioSource aud;
@@ -16,6 +17,9 @@ public class ShieldStats : MonoBehaviour, IDamage
 
     bool playerInShieldRange;
     bool shieldDead = false;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
