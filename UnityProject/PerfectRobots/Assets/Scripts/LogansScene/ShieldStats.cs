81ef75060e7a51af831d83569bc75993f0025c25
LogMoy32 <123963485+LogMoy32@users.noreply.github.com>
2023-11-16 12:14:18 -0500

    Fixed Background Music
    music was turned back on

--- UnityProject/PerfectRobots/Assets/Scripts/LogansScene/ShieldStats.cs
+++ UnityProject/PerfectRobots/Assets/Scripts/LogansScene/ShieldStats.cs
@@ -7,7 +7,6 @@ public class ShieldStats : MonoBehaviour, IDamage
 {
     [Header("===== Components =====")]
     [SerializeField] MeshRenderer model;
-    [SerializeField] GameObject shieldPos;
     [SerializeField] AudioSource aud;
 
     [Header("===== Stats =====")]
